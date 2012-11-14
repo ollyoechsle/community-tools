d9c606e7cb45315d7ffe7c1eb3198484c682b0a5
Olly Oechsle <owksley@gmail.com>
2012-11-14 23:19:51 +0000

    Added twitter search

---
+++ twitter.py
@@ -0,0 +1,18 @@
+import json
+import common
+import webapp2
+import logging
+import time
+from xml.dom import minidom
+
+def get_tweets(url):
+    json = common.get_cached_data(url)
+    return json
+
+class Twitter(webapp2.RequestHandler):
+    def get(self):
+        content = get_tweets("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=BreckCouncil")
+        #content = get_tweets("https://api.twitter.com/1.1/search/tweets.json?q=from:BreckCouncil")
+        common.write_response(self.request, self.response, content)
+
+app = webapp2.WSGIApplication([('/twitter', Twitter)], debug=True)
