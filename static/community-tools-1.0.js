5186223ead806861f097c4ad579a5dc31da7768b
Olly Oechsle <owksley@gmail.com>
2012-12-02 18:37:12 +0000

    Displaying weather type

--- static/community-tools-1.0.js
+++ static/community-tools-1.0.js
@@ -540,45 +540,45 @@ window.yaxham.modules = window.yaxham.modules || {};
     };
 
     WeatherModel.WEATHER = {
-        "NA":{"Name":"Not Available", img:""},
-        "0":{"Name":"Clear night", img:"01n.png"},
-        "1":{"Name":"Sunny day", img:"01d.png"},
-        "2":{"Name":"Partly cloudy", img:"01n.png"},
-        "3":{"Name":"Partly cloudy", img:"01d.png"},
-        "5":{"Name":"Mist", img:"15.png"},
-        "6":{"Name":"Fog", img:"15.png"},
-        "7":{"Name":"Cloudy", img:"04.png"},
-        "8":{"Name":"Overcast", img:"04.png"},
+        "NA":{"name":"Not Available", img:""},
+        "0":{"name":"Clear", img:"01n.png"},
+        "1":{"name":"Sunny", img:"01d.png"},
+        "2":{"name":"Partly cloudy", img:"01n.png"},
+        "3":{"name":"Partly cloudy", img:"01d.png"},
+        "5":{"name":"Mist", img:"15.png"},
+        "6":{"name":"Fog", img:"15.png"},
+        "7":{"name":"Cloudy", img:"04.png"},
+        "8":{"name":"Overcast", img:"04.png"},
 
-        "9":{"Name":"Light rain shower", img:"05n.png"},
-        "10":{"Name":"Light rain shower", img:"05d.png"},
+        "9":{"name":"Light rain shower", img:"05n.png"},
+        "10":{"name":"Light rain shower", img:"05d.png"},
 
-        "11":{"Name":"Drizzle", img:"09.png"},
-        "12":{"Name":"Light rain", img:"09.png"},
+        "11":{"name":"Drizzle", img:"09.png"},
+        "12":{"name":"Light rain", img:"09.png"},
 
-        "13":{"Name":"Heavy rain shower", img:"10.png"},
-        "14":{"Name":"Heavy rain shower", img:"10.png"},
-        "15":{"Name":"Heavy rain", img:"10.png"},
+        "13":{"name":"Heavy rain shower", img:"10.png"},
+        "14":{"name":"Heavy rain shower", img:"10.png"},
+        "15":{"name":"Heavy rain", img:"10.png"},
 
-        "16":{"Name":"Sleet shower", img:"12.png"},
-        "17":{"Name":"Sleet shower", img:"12.png"},
-        "18":{"Name":"Sleet", img:"12.png"},
+        "16":{"name":"Sleet shower", img:"12.png"},
+        "17":{"name":"Sleet shower", img:"12.png"},
+        "18":{"name":"Sleet", img:"12.png"},
 
-        "19":{"Name":"Hail shower", img:"08d.png"},
-        "20":{"Name":"Hail shower", img:"08n.png"},
-        "21":{"Name":"Hail", img:"08d.png"},
+        "19":{"name":"Hail shower", img:"08d.png"},
+        "20":{"name":"Hail shower", img:"08n.png"},
+        "21":{"name":"Hail", img:"08d.png"},
 
-        "22":{"Name":"Light snow shower", img:"08d.png"},
-        "23":{"Name":"Light snow shower", img:"08n.png"},
-        "24":{"Name":"Light snow", img:"08d.png"},
+        "22":{"name":"Light snow shower", img:"08d.png"},
+        "23":{"name":"Light snow shower", img:"08n.png"},
+        "24":{"name":"Light snow", img:"08d.png"},
 
-        "25":{"Name":"Heavy snow shower", img:"13.png"},
-        "26":{"Name":"Heavy snow shower", img:"13.png"},
-        "27":{"Name":"Heavy snow", img:"13.png"},
+        "25":{"name":"Heavy snow shower", img:"13.png"},
+        "26":{"name":"Heavy snow shower", img:"13.png"},
+        "27":{"name":"Heavy snow", img:"13.png"},
 
-        "28":{"Name":"Thunder shower", img:"11.png"},
-        "29":{"Name":"Thunder shower", img:"11.png"},
-        "30":{"Name":"Thunder", img:"11.png"}
+        "28":{"name":"Thunder shower", img:"11.png"},
+        "29":{"name":"Thunder shower", img:"11.png"},
+        "30":{"name":"Thunder", img:"11.png"}
     };
 
     yaxham.modules.WeatherModel = WeatherModel;
