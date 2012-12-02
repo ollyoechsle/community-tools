fa2533bca082e77d6989c4b652a4e4a7d53b220a
Olly Oechsle <owksley@gmail.com>
2012-12-02 08:02:06 +0000

    Added a bunch of weather icons

--- src/weather-model.js
+++ src/weather-model.js
@@ -19,6 +19,48 @@
         this.location = json.SiteRep.DV.Location;
     };
 
+    var w = {
+        "NA":{"Name":"Not Available", img:""},
+        "0":{"Name":"Clear night", img:"01n.png"},
+        "1":{"Name":"Sunny day", img:"01d.png"},
+        "2":{"Name":"Partly cloudy", img:"01n.png"},
+        "3":{"Name":"Partly cloudy", img:"01d.png"},
+        "5":{"Name":"Mist", img:"15.png"},
+        "6":{"Name":"Fog", img:"15.png"},
+        "7":{"Name":"Cloudy", img:"04.png"},
+        "8":{"Name":"Overcast", img:"04.png"},
+
+        "9":{"Name":"Light rain shower", img:"05n.png"},
+        "10":{"Name":"Light rain shower", img:"05d.png"},
+
+        "11":{"Name":"Drizzle", img:"09.png"},
+        "12":{"Name":"Light rain", img:"09.png"},
+
+        "13":{"Name":"Heavy rain shower", img:"10.png"},
+        "14":{"Name":"Heavy rain shower", img:"10.png"},
+        "15":{"Name":"Heavy rain", img:"10.png"},
+
+        "16":{"Name":"Sleet shower", img:"12.png"},
+        "17":{"Name":"Sleet shower", img:"12.png"},
+        "18":{"Name":"Sleet", img:"12.png"},
+
+        "19":{"Name":"Hail shower", img:"08d.png"},
+        "20":{"Name":"Hail shower", img:"08n.png"},
+        "21":{"Name":"Hail", img:"08d.png"},
+
+        "22":{"Name":"Light snow shower", img:"08d.png"},
+        "23":{"Name":"Light snow shower", img:"08n.png"},
+        "24":{"Name":"Light snow", img:"08d.png"},
+
+        "25":{"Name":"Heavy snow shower", img:"13.png"},
+        "26":{"Name":"Heavy snow shower", img:"13.png"},
+        "27":{"Name":"Heavy snow", img:"13.png"},
+
+        "28":{"Name":"Thunder shower", img:"11.png"},
+        "29":{"Name":"Thunder shower", img:"11.png"},
+        "30":{"Name":"Thunder", img:"11.png"}
+    };
+
     yaxham.modules.WeatherModel = WeatherModel;
 
 })();
