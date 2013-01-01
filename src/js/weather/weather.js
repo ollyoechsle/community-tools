44c94854fae63c7902bfee1c84af9cf327e8ed46
Olly Oechsle <owksley@gmail.com>
2013-01-01 12:19:54 +0000

    Supports multiple component types

--- src/js/weather/weather.js
+++ src/js/weather/weather.js
@@ -1,12 +1,24 @@
 (function () {
 
-    yaxham.modules.Weather = function(element) {
+    function getView(element, model) {
+        if (element.hasClass("horizontal")) {
+            return new yaxham.modules.WeatherChartView(element, model)
+        } else {
+            return new yaxham.modules.WeatherView(element, model)
+        }
+    }
 
-        var model = new yaxham.modules.WeatherModel(),
-            view = new yaxham.modules.WeatherChartView(element, model),
-            controller = new yaxham.modules.WeatherController(view, model);
+    yaxham.modules.Weather = function (selector) {
 
-        return controller;
+        jQuery(selector).each(function () {
+
+            var model = new yaxham.modules.WeatherModel(),
+                view = getView(jQuery(this), model),
+                controller = new yaxham.modules.WeatherController(view, model);
+
+            controller.initialise();
+
+        });
 
     }
 
