af56b8cd6a48a3eb52752273793cd83b8f38f321
ollyoechsle <owksley@gmail.com>
2022-02-16 18:04:00 +0000

    Implement basic news component

---
+++ frontend/components-vue2/src/components/News.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="news-component">
+    <ul class="news">
+      <li v-for="(article, index) in news" :key="index">
+        <a target="_blank" :href="article.link">{{ article.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import axios, {AxiosResponse} from 'axios'
+
+@Component
+export default class HelloWorld extends Vue {
+  private news: any = []
+
+  public mounted() {
+    axios.get('http://localhost:8080/news').then((response: AxiosResponse<any>) => {
+      this.news = response.data
+    })
+  }
+}
+</script>
